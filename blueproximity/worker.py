af95cef6df4054914b7976e93c20f38bdf865828
Mattster <mats-hjalmar.murman@alster.se>
2023-02-08 09:03:13 +0100

    Update to active lockscreen + phone nearby = unlock

--- blueproximity/worker.py
+++ blueproximity/worker.py
@@ -57,7 +57,7 @@ class Worker(threading.Thread):
                 logger.debug("checking if we are on locked screen and phone nearby")
                 status = subprocess.check_output("gnome-screensaver-command -q".split())
                 logger.debug(status)
-                if status == b'The screensaver is inactive\n':
+                if status == b'The screensaver is active\n':
                     logger.debug("Unlocking...")
                     subprocess.run(state.command.split())
 
